3fdb127a7740d7c5717beefd55177eca7d65700e
freelancer1845 <jaschelite@googlemail.com>
2017-10-05 20:50:35 +0200

    Added Jars needed for teneo hibernate

--- bundles/at.sunplugged.celldatabase.excelwriter.ui/src/at/sunplugged/celldatabase/excelwriter/ui/wizards/summary/SummaryExportWizard.java
+++ bundles/at.sunplugged.celldatabase.excelwriter.ui/src/at/sunplugged/celldatabase/excelwriter/ui/wizards/summary/SummaryExportWizard.java
@@ -2,55 +2,56 @@ package at.sunplugged.celldatabase.excelwriter.ui.wizards.summary;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
+
 import at.sunplugged.celldatabase.excelwriter.api.ExcelOutputHelper;
 import datamodel.Database;
 
 public class SummaryExportWizard extends Wizard {
 
-  private static final String WINDOW_TITLE = "Summary Export Wizard";
+	private static final String WINDOW_TITLE = "Summary Export Wizard";
 
-  private final Database database;
+	private final Database database;
 
-  protected PageOne pageOne;
+	protected PageOne pageOne;
 
-  public SummaryExportWizard(Database database) {
-    super();
+	public SummaryExportWizard(Database database) {
+		super();
 
-    this.database = EcoreUtil.copy(database);
-    setNeedsProgressMonitor(true);
-  }
+		this.database = EcoreUtil.copy(database);
+		setNeedsProgressMonitor(true);
+	}
 
-  @Override
-  public void addPages() {
-    pageOne = new PageOne(database);
+	@Override
+	public void addPages() {
+		pageOne = new PageOne(database);
 
-    addPage(pageOne);
-  }
+		addPage(pageOne);
+	}
 
-  @Override
-  public String getWindowTitle() {
-    return WINDOW_TITLE;
-  }
+	@Override
+	public String getWindowTitle() {
+		return WINDOW_TITLE;
+	}
 
-  @Override
-  public boolean performFinish() {
-    FileDialog dialog = new FileDialog(getShell(), SWT.SAVE);
-    dialog.setFilterExtensions(new String[] {"*.xlsx"});
-    dialog.setFilterNames(new String[] {"Ms Excel Open XML Format Spreadsheet"});
+	@Override
+	public boolean performFinish() {
+		FileDialog dialog = new FileDialog(getShell(), SWT.SAVE);
+		dialog.setFilterExtensions(new String[] { "*.xlsx" });
+		dialog.setFilterNames(new String[] { "Ms Excel Open XML Format Spreadsheet" });
 
-    if (dialog.open() != null) {
-      Path filePath = Paths.get(dialog.getFilterPath(), dialog.getFileName());
-      ExcelOutputHelper helper = new ExcelOutputHelper(database.getCellGroups(), filePath);
+		if (dialog.open() != null) {
+			Path filePath = Paths.get(dialog.getFilterPath(), dialog.getFileName());
+			ExcelOutputHelper helper = new ExcelOutputHelper(database.getCellGroups(), filePath);
+			helper.execute();
 
+		}
 
-    }
-
-
-    return false;
-  }
+		return false;
+	}
 
 }
